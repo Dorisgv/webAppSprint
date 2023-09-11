9a7f704a744acdd8adb03d039e86907187d05e49
Doris <guerrerodoris08@gmail.com>
2023-09-11 18:38:33 -0500

    User register

--- SweetAlert/validacionCampos.js
+++ SweetAlert/validacionCampos.js
@@ -1,7 +1,7 @@
 import { getContact } from "../api.js";
 
-
 const buttons = document.getElementById('formLogin');
+const formRegister = document.getElementById('formRegister'); //no se si me la coga.
 
 buttons.addEventListener("submit", async(e)=>{
     e.preventDefault();
@@ -24,3 +24,44 @@ buttons.addEventListener("submit", async(e)=>{
 });
 
 
+
+formRegister.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+    const url = 'http://localhost:3000'
+
+    const inputName = document.getElementById('inputName');
+    const inputCelular = document.getElementById('inputCelular');
+    const inputCont = document.getElementById('inputCont');
+    const inputUrl = document.getElementById('inputUrl'); 
+
+    const register = {
+
+        "nombre": inputName.value,
+        "telefono": inputCelular.value,
+        "imagen" : inputUrl.value,
+        "contraseña": inputCont.value,
+    }
+    try {
+        const response = await axios.post('http://localhost:3000/usuarios', register)
+        swal.fire('Registro exitoso')
+    } catch (error) {
+        
+    }
+        inputName.value,
+        inputCelular.value,
+        inputUrl.value,
+        inputCont.value
+})
+
+const Createyouraccount = document.querySelector('.buttons1');
+Createyouraccount.addEventListener('click', function(){
+   document.querySelector('.form-sign-up').style.display = 'block';
+   document.querySelector('.form-login').style.display = 'none';
+});
+
+const closemodal = document.querySelector('.close-modal');
+
+closemodal.addEventListener('click', function(){
+   document.querySelector('.form-sign-up').style.display = 'none';
+   document.querySelector('.form-login').style.display = 'block';
+});
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 import { getContact } from "./api.js";
 
-const modal_contact = document.getElementById('contact');
-const chatContact = document.getElementById('chatContact');//Es el evento de traer los contactos
-const contactosWhtapp = document.getElementById('contactos-whtapp');
-const containerChat = document.getElementById('container-chat');
-const anterior = document.getElementById('anterior');//Boton regresar
-const containerMensajes = document.getElementById('container__Mensajes');
-const imagenFondo = document.getElementById('imagen-fondo');
-const perfilCard = document.getElementById('perfil-card');
+const modal_contact = document.getElementById("contact");
+const chatContact = document.getElementById("chatContact"); //Es el evento de traer los contactos
+const contactosWhtapp = document.getElementById("contactos-whtapp");
+const containerChat = document.getElementById("container-chat");
+const anterior = document.getElementById("anterior"); //Boton regresar
+const containerMensajes = document.getElementById("container__Mensajes");
+const imagenFondo = document.getElementById("imagen-fondo");
+const perfilCard = document.getElementById("perfil-card");
 
 async function contact() {
-    containerChat.style.display = 'none'; //No se muestre
-    modal_contact.style.display = "block"; //Si se muestre
-    const contact = await getContact();
-    console.log(contact);
-    contact.forEach((product) =>  {
-        perfilCard.innerHTML +=`
+  containerChat.style.display = "none"; //No se muestre
+  modal_contact.style.display = "block"; //Si se muestre
+  const contact = await getContact();
+  console.log(contact);
+  contact.forEach((product) => {
+    perfilCard.innerHTML += `
         <div class="contact-card__perfil" id="contact-card" name="${product.id}">
                 <div class="contact-card__perfil__image" >
                 <img src="${product.imagen}" alt="${product.nombre}" />
@@ -23,75 +23,78 @@ async function contact() {
                 <span class="contact-card__perfil__name">${product.nombre}</span>
         </div>
             `;
+  });
+  const contactCard = document.getElementsByClassName("contact-card__perfil");
+
+  Array.from(contactCard).forEach((element) => {
+    let idUser = element.getAttribute("name");
+    element.addEventListener("click", () => {
+      imagenFondo.style.display = "none";
+      containerMensajes.style.display = "block";
+      console.log(idUser);
+
+      let infoContac = contact.find((item) => item.id == idUser);
+      // console.log(infoContac);
+      renderChat(infoContac);
     });
-    const contactCard = document.getElementsByClassName('contact-card__perfil');
-    
-        Array.from(contactCard).forEach((element) => {
-            let idUser = element.getAttribute('name');
-            element.addEventListener('click', () => {
-            imagenFondo.style.display = "none";
-        containerMensajes.style.display = "block";
-        console.log(idUser);
-        
-        let infoContac = contact.find(item => item.id == idUser);
-        // console.log(infoContac);
-        renderChat(infoContac);
-    });
-});
-}       
+  });
+}
 
-anterior.addEventListener('click', () => {
-    containerChat.style.display = 'block'; //
-    modal_contact.style.display = "none"; //
-    
+anterior.addEventListener("click", () => {
+  containerChat.style.display = "block"; //
+  modal_contact.style.display = "none"; //
 });
 
-chatContact.addEventListener('click', contact);
+chatContact.addEventListener("click", contact);
 
-const Createyouraccount = document.querySelector('.buttons1');
-Createyouraccount.addEventListener('click', function(){
-   document.querySelector('.form-sign-up').style.display = 'block';
-   document.querySelector('.form-login').style.display = 'none';
+/* const formLogin = document.getElementById("formLogin");
+const signupModal = document.getElementById("signupModal");
+const Createyouraccount = document.getElementById("create");
+
+Createyouraccount.addEventListener("click", function () {
+  formLogin.style.display = "none";
+  signupModal.style.display = "block";
 });
 
-const closemodal = document.querySelector('.close-modal');
+const closemodal = document.querySelector(".close-modal");
 
-closemodal.addEventListener('click', function(){
-   document.querySelector('.form-sign-up').style.display = 'none';
-   document.querySelector('.form-login').style.display = 'block';
-});
-
+closemodal.addEventListener("click", function () {
+  document.getElementById(".form-sign-up").style.display = "none";
+  document.getElementById(".form-login").style.display = "block";
+}); */
 
 async function renderChat(infoContac) {
-console.log(infoContac.nombre);
-infoUser.innerHTML += `
+  console.log(infoContac.nombre);
+  infoUser.innerHTML += `
     <img src="${infoContac.imagen}" alt=""> 
     <span>${infoContac.nombre}</span>
-`   
+`;
 }
 
 function enviarMensaje() {
-    const addProducto = async (e) => {
-        e.preventDefault()
-            const updatedData = {
-            "idUser1": nombreProducto.value, //capturar inicio de sesion
-            "idUser2": precioProducto.value, //Iniciar conversacion
-            "mensaje": [{ //Array de conversaciones
-                "sendBy": imagen0.value,
-                "fecha": imagen1.value,
-                "hora": imagen2.value,
-                "texto": imagen3.value,
-            }],
-            
-        };
-        
-        try {
-            const response = await axios.post(`${URL}/productos`, updatedData);
-            alert('Producto agreado:', response.data);
-            console.log(response);
-            } catch (error) {
-            console.log(error);
-            }
-            listProducts(currentCollection);
-        }
+  const addProducto = async (e) => {
+    e.preventDefault();
+    const updatedData = {
+      idUser1: nombreProducto.value, //capturar inicio de sesion
+      idUser2: precioProducto.value, //Iniciar conversacion
+      mensaje: [
+        {
+          //Array de conversaciones
+          sendBy: imagen0.value,
+          fecha: imagen1.value,
+          hora: imagen2.value,
+          texto: imagen3.value,
+        },
+      ],
+    };
+
+    try {
+      const response = await axios.post(`${URL}/productos`, updatedData);
+      alert("Producto agreado:", response.data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+    listProducts(currentCollection);
+  };
 }
